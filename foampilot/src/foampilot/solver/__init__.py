c425bc46a693f3ca01fbd0e9560b3c9df8ec9c8a
stevendaix <146651828+stevendaix@users.noreply.github.com>
2025-09-28 15:48:56 +0200

    Update __init__.py

--- foampilot/src/foampilot/solver/__init__.py
+++ foampilot/src/foampilot/solver/__init__.py
@@ -1,15 +1,17 @@
-# foampilot/solver/__init__.py
-
-# Importer les classes de solvers
+# Importer les classes de solvers spécifiques (pour usage interne ou avancé)
 from foampilot.solver.base_solver import BaseSolver
 from foampilot.solver.incompressible_fluid import incompressibleFluid
 from foampilot.solver.fluid import Fluid
 from foampilot.solver.incompressible_vof import IncompressibleVoF
 from foampilot.solver.solid import Solid
 
+# Importer la classe Solver générique (interface principale pour l'utilisateur)
+from foampilot.solver.solver import Solver
+
 # Liste des classes disponibles pour une utilisation facile
 __all__ = [
-    "BaseSolver",
+    "Solver",  # Interface principale recommandée
+    "BaseSolver",  # Pour un usage avancé ou interne
     "incompressibleFluid",
     "Fluid",
     "IncompressibleVoF",
