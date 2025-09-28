543f5884102c338501a5ffe9a7e13bb914332a0d
stevendaix <146651828+stevendaix@users.noreply.github.com>
2025-09-28 15:27:34 +0200

    Update __init__.py

--- foampilot/src/foampilot/solver/__init__.py
+++ foampilot/src/foampilot/solver/__init__.py
@@ -1,4 +1,17 @@
-# base/__init__.py
+# foampilot/solver/__init__.py
 
-# Importer les classes principales de test_solver et meshing
+# Importer les classes de solvers
+from foampilot.solver.base_solver import BaseSolver
 from foampilot.solver.incompressible_fluid import incompressibleFluid
+from foampilot.solver.fluid import Fluid
+from foampilot.solver.incompressible_vof import IncompressibleVoF
+from foampilot.solver.solid import Solid
+
+# Liste des classes disponibles pour une utilisation facile
+__all__ = [
+    "BaseSolver",
+    "incompressibleFluid",
+    "Fluid",
+    "IncompressibleVoF",
+    "Solid",
+]
