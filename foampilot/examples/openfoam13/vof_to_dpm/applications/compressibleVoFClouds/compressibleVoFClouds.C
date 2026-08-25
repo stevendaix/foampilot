732f7782383f37e6978762691474fcb261abddca
stevendaix <1.46651828e+08+stevendaix@users.noreply.github.com>
2026-08-25 07:08:48 +0000

    feat: detect configured liquid phase in compressible VoF cloud

--- foampilot/examples/openfoam13/vof_to_dpm/applications/compressibleVoFClouds/compressibleVoFClouds.C
+++ foampilot/examples/openfoam13/vof_to_dpm/applications/compressibleVoFClouds/compressibleVoFClouds.C
@@ -69,8 +69,38 @@ Foam::fv::compressible::compressibleVoFClouds::compressibleVoFClouds
         mu_,
         g_
     ),
+    liquidPhase_
+    (
+        dict.lookupOrDefault<word>
+        (
+            "liquidPhase",
+            mixture_.alpha1().group()
+        )
+    ),
+    liquidAlpha_
+    (
+        liquidPhase_ == mixture_.alpha1().group()
+      ? mixture_.alpha1()
+      : mixture_.alpha2()
+    ),
+    alphaThreshold_(dict.lookupOrDefault<scalar>("alphaThreshold", 0.5)),
+    minCells_(dict.lookupOrDefault<label>("minCells", 1)),
+    minVolume_(dict.lookupOrDefault<scalar>("minVolume", 0)),
+    detectFragments_(dict.lookupOrDefault<Switch>("detectFragments", true)),
     curTimeIndex_(-1)
-{}
+{
+    if
+    (
+        liquidPhase_ != mixture_.alpha1().group()
+     && liquidPhase_ != mixture_.alpha2().group()
+    )
+    {
+        FatalErrorInFunction
+            << "liquidPhase must be one of "
+            << mixture_.alpha1().group() << " or "
+            << mixture_.alpha2().group() << exit(FatalError);
+    }
+}
 
 Foam::wordList
 Foam::fv::compressible::compressibleVoFClouds::addSupFields() const
@@ -86,6 +116,32 @@ void Foam::fv::compressible::compressibleVoFClouds::correct()
     }
 
     mu_ = mixture_.rho()*mixture_.nu();
+    if (detectFragments_)
+    {
+        const volVectorField& U = mesh().lookupObject<volVectorField>("U");
+        const List<vofFragmentTransitionRecord> fragments =
+            vofFragmentTransition::detect
+            (
+                liquidAlpha_,
+                U,
+                alphaThreshold_,
+                minCells_,
+                minVolume_
+            );
+        scalar detectedVolume = 0;
+        forAll(fragments, fragmentI)
+        {
+            detectedVolume += fragments[fragmentI].volume;
+        }
+        Info<< "VOF fragments detected: " << fragments.size()
+            << ", convertible volume: " << detectedVolume << nl;
+        forAll(fragments, fragmentI)
+        {
+            Info<< "  fragment " << fragmentI
+                << " id " << fragments[fragmentI].id
+                << " volume " << fragments[fragmentI].volume << nl;
+        }
+    }
     clouds_.evolve();
     curTimeIndex_ = mesh().time().timeIndex();
 }
