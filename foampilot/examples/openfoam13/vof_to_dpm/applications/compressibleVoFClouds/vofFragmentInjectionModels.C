01b8496603787dbd835a9248dd3a73910c0d8dd1
stevendaix <1.46651828e+08+stevendaix@users.noreply.github.com>
2026-08-25 07:39:04 +0000

    build: register VOF injection for thermoCloud

--- foampilot/examples/openfoam13/vof_to_dpm/applications/compressibleVoFClouds/vofFragmentInjectionModels.C
+++ foampilot/examples/openfoam13/vof_to_dpm/applications/compressibleVoFClouds/vofFragmentInjectionModels.C
@@ -1,5 +1,8 @@
 #include "collidingCloud.H"
+#include "thermoCloud.H"
 #include "makeParcelInjectionModels.H"
+#include "makeThermoParcelInjectionModels.H"
 #include "vofFragmentInjection.H"
 
 makeInjectionModelType(vofFragmentInjection, collidingCloud);
+makeInjectionModelType(vofFragmentInjection, thermoCloud);
